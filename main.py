ee5f34fc8a0fbaa4d7175621f2147774d4b1f173
Marilaisanchez <marilaisanchez28@gmail.com>
2024-12-04 20:34:52 -0600

    Creating main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
             "XD": " Risa intensa",
-            "Fail": "fracaso",
-            "Red flag": " Se usa como una alerta cuando alguien tiene actitudes consideradas negativas",
-            "Green falg": "Una persona con actitudes o rasgos positivos que pueden indicar que una relación potencial será sana",
-            "Cancelad@": "Atacar o descalificar los puntos de vista de otra persona",
-            "Petite": "Algo pequeño o ser pequeño",
+            "FAIL": "Fracaso",
+            "RED FLAG": " Se usa como una alerta cuando alguien tiene actitudes consideradas negativas",
+            "GREEN FLAG": "Una persona con actitudes o rasgos positivos que pueden indicar que una relación potencial será sana",
+            "CANCELAD@": "Atacar o descalificar los puntos de vista de otra persona",
+            "PETITE": "Algo pequeño o ser pequeño",
             "POV": "Punto de vista o desde la perspectiva de alguien",
             "SIMP": "referirse a una persona que está desesperado por la atención y el afecto de otra persona",
             }
@@ -23,3 +23,4 @@ elif word == "SORPRENDEME":
     print("La palabra est", wordr, "y el significado es:", meme_dict[wordr])
 else:
     print("Esta palabra aún no se ha agregado al diccionario")
+
